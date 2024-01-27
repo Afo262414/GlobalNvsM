172f5aa113d542647ead7a93a8bedbe77ce14b2f
Afo262414 <idv20c.aflores@uartesdigitales.edu.mx>
2024-01-27 05:56:19 -0600

    Fixed the problem whit fmod and solved the issue whit the sound

---
+++ Assets/AfoScripts/SecondWander.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondWander : MonoBehaviour
+{
+  public Transform[] waypoints; // Array of waypoints the Whitexican will follow
+  public int currentWaypointIndex = 0; // Index of the current waypoint
+  public float moveSpeed = 5f; // Speed at which the Whitexican moves
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    currentWaypointIndex = 0;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Move towards the current waypoint
+    if (currentWaypointIndex < waypoints.Length)
+    {
+      Vector3 targetPosition = waypoints[currentWaypointIndex].position;
+      transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+      // Check if the enemy has reached the current waypoint
+      if (transform.position == targetPosition)
+      {
+        currentWaypointIndex++;
+        if (currentWaypointIndex >= 2)
+        {
+          currentWaypointIndex = 0;
+        }
+      }
+      // Check if the move speed is lees of 1f to change the waypoint
+      if (moveSpeed < 1f)
+      {
+        currentWaypointIndex++;
+        if (currentWaypointIndex >= 2)
+        {
+          currentWaypointIndex = 0;
+        }
+      }
+    }
+  }
+}
