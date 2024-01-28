a7f4737272e185cac273f10e1a2a032c45293d17
Vonchirrina <ldv23c.imeza@uartesdigitales.edu.mx>
2024-01-27 19:08:34 -0600

    UI Terminada
    ++ Canvas Menu
    ++Canvas Pausa
    ++Canvas Creditos

---
+++ Assets/Ivonne/Scripts/Menu_inicial.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_inicial : MonoBehaviour
+{
+    public string gameScene;
+    public string creditScene;
+    public string MainMenuScene;
+
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void Salir()
+    {
+        //Debug.Log("Salir...");
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(creditScene);
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene(MainMenuScene);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(MainMenuScene);
+    }
+
+    public void Continue()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+}
