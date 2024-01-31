932d1489f68a96ffe4003dbf0f9048a6b0a102ad
CaptainMamon <151094540+TrashCandys@users.noreply.github.com>
2024-01-31 16:14:46 -0600

    Horse

--- Assets/Plaayer_Controller_Nahuatl.cs
+++ Assets/Plaayer_Controller_Nahuatl.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float acceleration = 2f;  // Aceleración del jugador
     public float maxSpeed = 10f;     // Velocidad máxima del jugador
     public AudioSource walk;
+    public Animator animator;
 
     private Rigidbody playerRigidbody;
     private bool isMoving = false;
@@ -21,10 +22,12 @@ public class PlayerController : MonoBehaviour
     {
         // Entrada del jugador
         float horizontalInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("Horizontal", Mathf.Abs(horizontalInput));
         float verticalInput = Input.GetAxis("Vertical");
 
         // Calcula la dirección del movimiento
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+
         movement.Normalize(); // Para evitar el movimiento más rápido en diagonal
 
         // Comprueba si el jugador se está moviendo
@@ -53,6 +56,24 @@ public class PlayerController : MonoBehaviour
         if (playerRigidbody.velocity.magnitude > maxSpeed)
         {
             playerRigidbody.velocity = playerRigidbody.velocity.normalized * maxSpeed;
+        }
+
+        // Gira el sprite en la dirección del movimiento
+        if (isMoving)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 0f);
+
+            // Voltea el sprite horizontalmente si te estás moviendo hacia la derecha
+            if (horizontalInput > 0)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            // Voltea el sprite horizontalmente si te estás moviendo hacia la izquierda
+            else if (horizontalInput < 0)
+            {
+                transform.localScale = new Vector3(-1, 1f, 1f);
+            }
         }
     }
 }
